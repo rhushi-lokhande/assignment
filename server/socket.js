fb4758e2a7c0a7e9e4268d5fae901e79a392b04a
Rhushikesh Lokhande <rhushikeshl@byteridge.com>
2017-12-03 10:11:36 +0530

    removed console log

--- server/socket.js
+++ server/socket.js
@@ -2,14 +2,11 @@ const _util = require('./util');
 const moment = require('moment');
 exports._socket = (io) => {
     io.sockets.on('connection', (socket) => {
-        console.log('Socket connected');
         let invterval;
         socket.on('getData', (data) => {
-            console.log(data);
             const mstart = moment(data.startDate, 'yyyy-mm-dd');
             const mend = moment(data.endDate, 'yyyy-mm-dd').endOf('day');
             invterval = setInterval(() => {
-                console.log('dd', mend, mstart);
                 if (mend > mstart) {
                     socket.emit('updateGraph', {
                         date: mstart.add(data.frequency, 'second').toString(),
